22a8c24bb3b5abcc8dc820e9791e7374342dd297
Christopher M. Pierce <contact@chris-pierce.com>
2025-03-20 13:52:06 -0700

    add remaining region commands

--- src/electronsandstuff/icool/__init__.py
+++ src/electronsandstuff/icool/__init__.py
@@ -1,6 +1,26 @@
 from .main import ICoolInput
-from .region_commands import Cell
+from .region_commands import (
+    Cell,
+    SRegion,
+    RefP,
+    Ref2,
+    DVar,
+    Grid,
+    Repeat,
+    CoolingSection,
+)
 from .exceptions import UnresolvedSubstitutionsError
 
 
-__all__ = (ICoolInput, Cell, UnresolvedSubstitutionsError)
+__all__ = (
+    ICoolInput,
+    Cell,
+    SRegion,
+    RefP,
+    Ref2,
+    DVar,
+    Grid,
+    Repeat,
+    CoolingSection,
+    UnresolvedSubstitutionsError,
+)
