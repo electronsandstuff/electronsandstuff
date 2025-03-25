0c3930b5b2c6dd47d8735504511787c92886acbd
Christopher M. Pierce <contact@chris-pierce.com>
2025-03-24 23:00:29 -0700

    add fields and geometry to init

--- src/electronsandstuff/icool/__init__.py
+++ src/electronsandstuff/icool/__init__.py
@@ -9,6 +9,26 @@ from .region_commands import (
     Repeat,
     CoolingSection,
 )
+from .fields import (
+    FieldAccel10,
+    FieldAccel2,
+    FieldNone,
+    FieldSol,
+    FieldSTUS,
+    ICoolField,
+)
+from .geometry import (
+    GeometryASPW,
+    GeometryASWR,
+    GeometryCBlock,
+    GeometryHWin,
+    GeometryNIA,
+    GeometryNone,
+    GeometryPWedge,
+    GeometryRing,
+    GeometryWedge,
+    ICoolGeometry,
+)
 from .exceptions import UnresolvedSubstitutionsError
 
 
@@ -22,5 +42,21 @@ __all__ = (
     Grid,
     Repeat,
     CoolingSection,
+    FieldAccel10,
+    FieldAccel2,
+    FieldNone,
+    FieldSol,
+    FieldSTUS,
+    ICoolField,
+    GeometryASPW,
+    GeometryASWR,
+    GeometryCBlock,
+    GeometryHWin,
+    GeometryNIA,
+    GeometryNone,
+    GeometryPWedge,
+    GeometryRing,
+    GeometryWedge,
+    ICoolGeometry,
     UnresolvedSubstitutionsError,
 )
